00df6a3c972366ad153413ce5070d7b61d95d63e
Igor Augusto Monteiro Silva <igoraugustoms91@gmail.com>
2020-04-25 11:30:14 -0300

    correction down + size of string

--- src/database/migrations/20200423173721_create_user.js
+++ src/database/migrations/20200423173721_create_user.js
@@ -2,10 +2,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', function (table){
         table.increments('cd_User');
-        table.string('nm_Login');
-        table.string('nm_Email');
-        table.string('nm_Type');
-        table.string('nm_Password');
+        table.string('nm_Login', 30);
+        table.string('nm_Email', 40);
+        table.string('nm_Type', 9);
+        table.string('nm_Password', 20);
         table.timestamp('dt_Register').defaultTo(knex.fn.now());
 
     });
@@ -13,5 +13,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('user');
+    return knex.schema.dropTable('users');
 };
