0f10b1fc20f91d85e98f0cc6e09bb3229c7b5473
Henrique Gaeta <henriquehg@gmail.com>
2020-06-20 02:46:24 -0300

    Adicionando sessão e primeiro acesso

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
  const express = require('express');
 
  const UserController = require('./app/controllers/UserController');
+ const SessionController = require('./app/controllers/SessionController');
 
  const routes = express.Router();
 
  // Routes
  routes.post('/auth/register', UserController.create);
  routes.delete('/auth/delete_account', UserController.delete);
- routes.post('/auth/reset_password',UserController.reset);
+ routes.post('/auth/first_acess', UserController.firstAcess);
+ routes.post('/auth/forgot_password',UserController.forgotPassword);
+ routes.post('/auth/recover_password',UserController.reset);
+ routes.post('/session/authenticate',SessionController.authenticateSession); 
 
  module.exports = routes;
