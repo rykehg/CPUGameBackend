00f4e846113e0d99548d86fa43bac82a8d940b57
Lucas Davi <lucasdavisan@gmail.com>
2020-10-23 11:47:53 -0300

    Correçao de bug referente ao acesso remoto a aplicaçao usando o CORS.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://cpu-game-frontend.herokuapp.com'
+}));
 app.use(express.json());
 app.use(routes);
 app.use(errors());
